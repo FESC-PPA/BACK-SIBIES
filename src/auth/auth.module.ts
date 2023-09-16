b7be97fabe27175dace2e30e93c9b4a0f26b62b3
jesusYanezx01 <116025036+jesusYanezx01@users.noreply.github.com>
2023-09-16 09:11:35 -0500

    Implementacion de Jwt al modulo de Auth y instalacion de dotenv

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
+import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_CLAVE,
+      signOptions: { expiresIn: '600s' },
+    }),
+  ],
 })
 export class AuthModule {}
